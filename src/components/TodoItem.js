21ca7df087b2e0a0e00345ad42d1fae929ec5179
nkrumahthis <nkrumahthis@gmail.com>
2020-11-27 12:17:46 +0000

    grab todo id and title in todoitem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -30,11 +30,12 @@ export default class TodoItem extends Component {
 
 
     render() {
+        const {id, title} = this.props.todo;
         return (
             <div style={this.getStyle()}>
                 <p>
-                    <input type="checkbox" onChange={this.props.markComplete.bind(this, this.props.todo.id)}/> {' '}
-                    {this.props.todo.title}
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    {title}
                 </p>
             </div>
         )
