5ca0e8ddb6f04da6e7d723629092cf9a6b5006d8
nkrumahthis <nkrumahthis@gmail.com>
2020-11-30 11:17:46 +0000

    ui of addtodo

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
 
 export class AddTodo extends Component {
+
+    state = {
+        title: ''
+    }
+
+    onChange = (e) => this.setState({title: e.target.value});
+
     render() {
         return (
-            <form style={{display: flex}}>
-                <input type="text" name="title" id="" placeholder="add Todo ..." style={{flex:'10'}}/>
-                <input type="submit" value="submit" className="btn" style={{flex:'1'}}/>
+            <form style={{display: 'flex'}}>
+                <input 
+                    type="text" 
+                    name="title" 
+                    id="" 
+                    placeholder="add Todo ..." 
+                    style={{flex:'10'}} 
+                    value = {this.state.title} 
+                    onChange={this.onChange}
+                    />
+                <input 
+                    type="submit" 
+                    value="submit" 
+                    className="btn" 
+                    style={{flex:'1'}}/>
             </form>
         )
     }
